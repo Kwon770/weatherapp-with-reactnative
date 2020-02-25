98bd8126f2ff8faeb3341c24d973bb4f1912c0d5
Kwon770 <sckwon770@gmail.com>
2020-02-25 16:35:44 +0900

    1-0 Layouts with flexbox in react native

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// View = div / Text = span, h1, h2, p
 export default function App() {
   return (
     <View style={styles.container}>
@@ -9,6 +10,11 @@ export default function App() {
   );
 }
 
+// Web default flex-diretion : row
+// ReactNative default flex-diretion : column
+
+// flex : 화면상에서 차지하는 비율
+// 1개 일경우 => 화면 꽉채울 / 2개 일경우 flex: 1 - flex:1 => 1:1 로 절반씩 채움
 const styles = StyleSheet.create({
   container: {
     flex: 1,
