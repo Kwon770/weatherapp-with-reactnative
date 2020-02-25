18dbe223f5b86ec3ba62449f3c88b5023e85fd19
Kwon770 <sckwon770@gmail.com>
2020-02-25 18:31:50 +0900

    2-1 Getting the condition names

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Alert} from "react-native";
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import Weather from "./Weather";
 import * as Location from "expo-location";
@@ -7,39 +7,43 @@ import axios from "axios";
 
 const API_KEY = "062aa5e38df65a32830c83c7940b0e0f";
 
-// View = div / Text = span, h1, h2, p
 export default class extends React.Component {
   state = {
     isLoading: true
-  }
-  getWeather = async(latitude, longitude) => {
-    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    this.setState({isLoading: false, temp: data.main.temp});
-    console.log(data);
-  }
-  getLocation = async() => {
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, condition: weather[0].main, temp });
+  };
+  getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const {coords : {latitude , longitude}} = await Location.getCurrentPositionAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
     }
-    catch (error) {
-      Alert.alert("Can't find you", "So sad")
-    }
-  }
+  };
 
   componentDidMount() {
     this.getLocation();
   }
 
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
-
-// Web default flex-diretion : row
-// ReactNative default flex-diretion : column
-
-// flex : 화면상에서 차지하는 비율
-// 1개 일경우 => 화면 꽉채울 / 2개 일경우 flex: 1 - flex:1 => 1:1 로 절반씩 채움
--- Weather.js
+++ Weather.js
@@ -1,21 +1,41 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
-export default function Weather({temp})  {
-    return <View style={styles.container}>
-    <Text>{temp}</Text>
+// View = div / Text = span, h1, h2, p
+export default function Weather({ temp, condition }) {
+  return (
+    <View style={styles.container}>
+      <Text>{temp}</Text>
     </View>
+  );
 }
 
 Weather.prototype = {
-    temp: PropTypes.number.isRequired
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust"
+  ]).isRequired
 };
 
+// Web default flex-diretion : row
+// ReactNative default flex-diretion : column
+
+// flex : 화면상에서 차지하는 비율
+// 1개 일경우 => 화면 꽉채울 / 2개 일경우 flex: 1 - flex:1 => 1:1 로 절반씩 채움
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
